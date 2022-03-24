5625a975e1e584f4a24bd34285bc68f981d0e062
stitou-nada <nadastito1087@gmail.com>
2022-03-24 11:48:19 +0000

    add prototype2-v1

---
+++ prototype-2/index.php
@@ -0,0 +1,33 @@
+
+<?php 
+include 'gestionProduit.php';
+$gestionProduit = new GestionProduit();
+$data= $gestionProduit->afficher();
+
+foreach($data as $value){
+
+
+   
+
+?>
+
+<div>
+<table border="1" width="20%">
+<tr>
+
+<td>
+    <a href="detail de produit.php?id=<?= $value->getId();?>">
+<?= $value->getNom();?></a> </td>
+</tr>
+
+
+
+
+</table>
+
+</div>
+
+
+<?php } ?>
+
+<a href="panier.php">panier</a>
