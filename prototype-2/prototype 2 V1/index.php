c72e4779cc324f3a3bbcee7993779ba7c8831e62
stitou-nada <nadastito1087@gmail.com>
2022-03-25 13:36:31 +0000

    version2

--- prototype-2/prototype 2 V1/index.php
+++ prototype-2/prototype 2 V1/index.php
@@ -1,6 +1,53 @@
 <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Shop Homepage - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#!">logo</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Acueile</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Promotion</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Magasin</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>  
+                    </ul>
+                    <form action="ajouter.php"method="POST" class="d-flex">
+                        <button   class="btn btn-outline-dark" type="submit">
+                         <i class="bi-cart-fill me-1" ></i>
+                            Cart
+                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">LES PRODUIT</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">Lorem ipsum dolor sit amet consectetur</p>
+                </div>
+            </div>
+        </header>
+        <!-- Section-->
+        <?php 
 include 'gestionProduit.php';
 $gestionProduit = new GestionProduit();
 $data= $gestionProduit->afficher();
@@ -8,18 +55,99 @@ $data= $gestionProduit->afficher();
 foreach($data as $value){
 
 ?>
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder"><?= $value->getNom();?></h5>
+                                    <!-- Product price-->
+                                    <?= $value->getPrix();?> DH
+                                </div>
+                            </div>
+                            <?php } ?>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a href="detail de produit.php?id=<?= $value->getId();?>"class="btn btn-outline-dark mt-auto" href="#">Détail</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                      
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder"><?= $value->getNom();?></h5>
+                                    
+                                  
+                                    <!-- Product price-->
+                                    <?= $value->getPrix();?> DH
+                                    
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a href="detail de produit.php?id=<?= $value->getId();?>" class="btn btn-outline-dark mt-auto" href="#">Détail</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                        
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder"><?= $value->getNom();?></h5>
+                                    <!-- Product price-->
+                                    <?= $value->getPrix();?> DH
+                                    
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a  href="detail de produit.php?id=<?= $value->getId();?>" class="btn btn-outline-dark mt-auto" href="#">Détail</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder"><?= $value->getNom();?></h5>
+                                  
+                                    <!-- Product price-->
+                                    <?= $value->getPrix();?> DH
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a href="detail de produit.php?id=<?= $value->getId();?>" class="btn btn-outline-dark mt-auto" href="#">Détail</a></div>
+                            </div>
+                        </div>
+                    </div>
 
-<div class="card " style="width: 18rem;">
-  <img src="../img/cta-2-368x420.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title"><?= $value->getNom();?></h5>
-    <p class="card-text"><?= $value->getPrix();?> dh</p>
-    <a href="detail de produit.php?id=<?= $value->getId();?>" class="btn btn-primary">Detail de produit</a>
-  </div>
-</div>
+    
+    </body>
+</html>
 
 
 
-<?php } ?>
 
-<a href="panier.php">panier</a>
+
